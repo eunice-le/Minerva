abd0ab85bee39c8b104b94080c06c7e7915efb0d
eunice-le <60023787+eunice-le@users.noreply.github.com>
2020-02-21 15:50:57 -0300

    Update CS112 Assignment 2.r

--- CS112 Assignment 2.r
+++ CS112 Assignment 2.r
@@ -27,6 +27,33 @@ abline(lm.control,
        lty = 2)
 
 ###Part (b)
+###Part (b)
+#Set up a new data set for modification 
+sesame.b <- read.csv("https://tinyurl.com/wlgl63b")
+sesame.b$post.test[c(16)]<-85
+#Separate treatment from control groups
+which.treat.b <- which(sesame.b$treatment== 1)
+which.control.b <- which(sesame.b$treatment == 0)
+sesame.treat.b <- sesame.b[-which.control,]
+sesame.control.b <- sesame.b[-which.treat,]
+#Linear regression between pre.test and post.test 
+lm.treat.b <- lm(post.test ~ pre.test, sesame.treat.b)
+lm.control.b <- lm(post.test ~ pre.test, sesame.control.b)
+lm.treat.b
+lm.control.b
+#Plotting the regression 
+plot(x = sesame.b$pre.test, y = sesame.b$post.test,
+     pch = ifelse(sesame.b$treatment == 1, 1, 16),
+     col = ifelse(sesame.b$post.test == 85, "blue", "black"),
+     main = "Grade 4",
+     xlab = "Pre-test", 
+     ylab = "Post-test")
+abline(lm.treat.b,
+       col = "black")
+abline(lm.control.b,
+       col = "black",
+       lty = 2)
+
 
 
 
